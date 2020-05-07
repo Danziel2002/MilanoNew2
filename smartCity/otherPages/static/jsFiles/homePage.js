79a01eb398b705702dae3b56fb7b59df498faf2e
Ipatie Daniel <gruescudaniel006@gmail.com>
2020-05-07 22:18:46 +0300

    qwe

--- smartCity/otherPages/static/jsFiles/homePage.js
+++ smartCity/otherPages/static/jsFiles/homePage.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-
+                $.ajax({
+                        type: "POST",
+                        url: "http://danziel2.pythonanywhere.com/account/giveExp/?username=Danziel&experience=0",
+                        success: function(data){
                                     $.cookie('username', data['username'], {path: '/' });
                                     $.cookie('exp', data['exp'], {path: '/' });
                                     $.cookie('level', data['level'], {path: '/' });
                                     $.cookie('recycle', data['recycle'], {path: '/'});
+                        }
+                        });
         $('.logOut').click(function() {
             $.removeCookie('username',{ path: '/' });
             $.removeCookie('exp',{ path: '/' });
