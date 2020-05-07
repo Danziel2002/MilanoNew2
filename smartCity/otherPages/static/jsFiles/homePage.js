57df5d04e16d1b20ad3b3a89a4853fdd7473dd7b
Ipatie Daniel <gruescudaniel006@gmail.com>
2020-05-07 21:56:23 +0300

    ASDas

---
+++ smartCity/otherPages/static/jsFiles/homePage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+                                    $.cookie('username', data['username'], {path: '/' });
+                                    $.cookie('exp', data['exp'], {path: '/' });
+                                    $.cookie('level', data['level'], {path: '/' });
+                                    $.cookie('recycle', data['recycle'], {path: '/'});
+ 
+
+});
+                
